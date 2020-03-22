603c9ecf4095f1cb88f6ac56f951b71e9aaf0c58
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2020-03-22 12:27:33 +0000

    Landing Page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,13 +3,17 @@ import { TopNav } from './TopNav/TopNav';
 import logo from '../assets/logo.png';
 import styles from './LandingPage.module.css'
 import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchSuggestions } from './SearchSuggestions/SearchSuggestions';
 
 export function LandingPage() {
     return (
-        <div>
-            <TopNav />
-            <img src={logo} className={styles.logo} alt='Logo' />
-            <SearchBar />
+        <div className={styles.landing}>
+            <div className={styles['search-area']}>
+                <TopNav />
+                <img src={logo} className={styles.logo} alt='Logo' />
+                <SearchBar />
+                <SearchSuggestions />
+            </div>
         </div>
     );
 }
